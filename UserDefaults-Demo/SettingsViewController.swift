a39e5206a0a19e39efc5fe81baf106c8ba0b195e
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-14 10:24:52 -0500

    made image storable

--- UserDefaults-Demo/SettingsViewController.swift
+++ UserDefaults-Demo/SettingsViewController.swift
@@ -9,49 +9,69 @@
 import UIKit
 
 class SettingsViewController: UITableViewController {
-  
-  @IBOutlet weak var unitMeasurementLabel: UILabel!
-  
-  var currentUnit = UnitMeasurement.miles {
     
-    didSet {
-      // update the unitMeasurement label
-      unitMeasurementLabel.text = currentUnit.rawValue // "Miles" or "Kilometers"
-      
-      // update value in user defaults
-      UserPreference.shared.updateUnitMeasurement(with: currentUnit)
+    @IBOutlet weak var unitMeasurementLabel: UILabel!
+    @IBOutlet var bikeRunImage: UIImageView!
+    
+    var currentUnit = UnitMeasurement.miles {
+        
+        didSet {
+            // update the unitMeasurement label
+            unitMeasurementLabel.text = currentUnit.rawValue // "Miles" or "Kilometers"
+            
+            // update value in user defaults
+            UserPreference.shared.updateUnitMeasurement(with: currentUnit)
+        }
     }
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    updateUI()
-  }
-  
-  private func updateUI() {
-    // retrieve any values in UserDefaults as needed
-    if let unitMeasurement = UserPreference.shared.getUnitMeasurement() {
-      currentUnit = unitMeasurement
+    var currentImage = BikeRun.bike {
+        
+        didSet {
+            bikeRunImage.image = UIImage(named: currentImage.rawValue) // "Miles" or "Kilometers"
+            
+            UserPreference.shared.updateImage(with: currentImage)
+        }
     }
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
-    // toggle between "Miles" or "Kilometers"
-    
-    switch indexPath.row {
-    case 0:
-      // toggle unit measurement label
-      toggleUnitMeasurement()
-    default:
-      break
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
     }
-  }
-  
-  private func toggleUnitMeasurement() {
-    currentUnit = (currentUnit == UnitMeasurement.miles) ? UnitMeasurement.kilometers : UnitMeasurement.miles
-  }
-  
-  
+    
+    private func updateUI() {
+        // retrieve any values in UserDefaults as needed
+        if let unitMeasurement = UserPreference.shared.getUnitMeasurement() {
+            currentUnit = unitMeasurement
+        }
+        
+        if let imageString = UserPreference.shared.getImageString() {
+            currentImage = imageString
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // toggle between "Miles" or "Kilometers"
+        
+        switch indexPath.row {
+        case 0:
+            // toggle unit measurement label
+            toggleUnitMeasurement()
+        case 1:
+            toggleImage()
+        default:
+            break
+        }
+    }
+    
+    private func toggleUnitMeasurement() {
+        currentUnit = (currentUnit == UnitMeasurement.miles) ? UnitMeasurement.kilometers : UnitMeasurement.miles
+    }
+    
+    private func toggleImage() {
+        currentImage = (currentImage == BikeRun.bike) ? BikeRun.run : BikeRun.bike
+    }
+    
+    
 }
--- UserDefaults-Demo/UserPreference.swift
+++ UserDefaults-Demo/UserPreference.swift
@@ -9,40 +9,57 @@
 import Foundation
 
 enum UnitMeasurement: String {
-  case miles = "Miles"
-  case kilometers = "Kilometers"
+    case miles = "Miles"
+    case kilometers = "Kilometers"
+}
+
+enum BikeRun: String {
+    case bike = "bike"
+    case run = "run"
 }
 
 struct UserPreferenceKey {
-  static let unitMeasurement = "Unit Measurement"
+    static let unitMeasurement = "Unit Measurement"
+    static let bikeRunImage = "Bike or Run"
 }
 
 class UserPreference {
-  
-  // a Singleton's initializer NEEDS to be private - this ensures that
-  // ONLY one instance of this class is used throughout the application
-  private init() {}
     
-  static let shared = UserPreference()
-  
-  // set a user defaults value (object)
-  func updateUnitMeasurement(with unit: UnitMeasurement) {
-    // storing or persiting the unit measurment value to UserDefaults (device or simimular as permanent storage)
+    // a Singleton's initializer NEEDS to be private - this ensures that
+    // ONLY one instance of this class is used throughout the application
+    private init() {}
     
-    // UserDefaults.standard is a Singleton in iOS that gives us access to
-    // saving and retrieving stored data in the device or simulator
-    UserDefaults.standard.set(unit.rawValue, forKey: UserPreferenceKey.unitMeasurement)
+    static let shared = UserPreference()
     
-    // key is "Unit Measurement"
-    // value is either "Miles" or "Kilometers"
-    // UserDefaults: ["Unit Measurement" : "Kilometers"]
-  }
-  
-  // retrieve a user defaults value (object)
-  func getUnitMeasurement() -> UnitMeasurement? {
-    guard let unitMeasurement = UserDefaults.standard.object(forKey: UserPreferenceKey.unitMeasurement) as? String else {
-      return nil
+    // set a user defaults value (object)
+    func updateUnitMeasurement(with unit: UnitMeasurement) {
+        // storing or persiting the unit measurment value to UserDefaults (device or simimular as permanent storage)
+        
+        // UserDefaults.standard is a Singleton in iOS that gives us access to
+        // saving and retrieving stored data in the device or simulator
+        UserDefaults.standard.set(unit.rawValue, forKey: UserPreferenceKey.unitMeasurement)
+        
+        // key is "Unit Measurement"
+        // value is either "Miles" or "Kilometers"
+        // UserDefaults: ["Unit Measurement" : "Kilometers"]
     }
-    return UnitMeasurement(rawValue: unitMeasurement)
-  }
+    
+    func updateImage(with image: BikeRun) {
+        UserDefaults.standard.set(image.rawValue, forKey: UserPreferenceKey.bikeRunImage)
+    }
+    
+    // retrieve a user defaults value (object)
+    func getUnitMeasurement() -> UnitMeasurement? {
+        guard let unitMeasurement = UserDefaults.standard.object(forKey: UserPreferenceKey.unitMeasurement) as? String else {
+            return nil
+        }
+        return UnitMeasurement(rawValue: unitMeasurement)
+    }
+    
+    func getImageString() -> BikeRun? {
+        guard let bikeOrRun = UserDefaults.standard.object(forKey: UserPreferenceKey.bikeRunImage) as? String else {
+            return nil
+        }
+        return BikeRun(rawValue: bikeOrRun)
+    }
 }
